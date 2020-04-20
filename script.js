3cba39c0a4f4d2b66afccd0b91d4380bc24aeede
oelliots <olgaschou@Olgas-MacBook-Pro.local>
2020-04-20 11:34:56 +0300

    updates and menu

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("info").onclick = function() {myFunction()};
+// Get the button, and when the user clicks on it, executes myFunction
+document.getElementById("info").onclick = function() {infoShow()};
+document.getElementById("index").onclick = function() {indexShow()};
+document.getElementsByClassName("index-project").onclick = function() {indexClose()};
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.getElementById("myInfo").classList.toggle("show");
+function infoShow() {
+  document.getElementById("myInfo").classList.toggle("show-info");
 }
+function indexShow() {
+  document.getElementById("myIndex").classList.toggle("show-index");
+}
+function indexClose() {
+  document.getElementById("myIndex").classList.toggle("close-index");
+}
+
+
 
 $(document).ready(function(){
   $('body').mousemove(function(e){
      var x = e.pageX - this.offsetLeft;
 
-    if (x <= 8) {
+    if (x <= 12) {
          $('div.ball1').css({'left': x});
       }
   });
@@ -19,7 +29,7 @@ $(document).ready(function(){
   $('body').mousemove(function(e){
      var x = e.pageX - this.offsetLeft;
 
-    if (x <= 8) {
+    if (x <= 12) {
          $('div.ball2').css({'left': x});
       }
   });
